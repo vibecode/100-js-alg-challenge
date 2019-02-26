cf04db47d31e5c8c1cf42f624d63b0a55c34c03d
vibecode <rumashkin@gmail.com>
2019-02-26 16:54:31 +0100

    add function  solution

--- add/add.js
+++ add/add.js
@@ -1,9 +1,12 @@
-function add(param1, param2) {
+function add() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return args.reduce(function (acc, num) { return acc + num; });
 }
-// function add2(param1: number[]): number {
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add(1, 2));
+console.log(add(3, 2));
+console.log(add(1, 2, 3, 4, 5));
+console.log(add(2, 3));
 //# sourceMappingURL=add.js.map
--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,9 @@
-function add(param1: number, param2: number): number {
-
+function add(...args: number[]): number {
+  return args.reduce((acc, num) => acc + num)
 }
 
-// function add2(param1: number[]): number {
+console.log(add(1, 2))
+console.log(add(3, 2))
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
-
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
-
+console.log(add(1, 2, 3, 4, 5))
+console.log(add(2, 3))
