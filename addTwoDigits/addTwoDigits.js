a365c854ba88a5b4b5a2fd8173f1c1a821a7d876
vibecode <rumashkin@gmail.com>
2019-03-01 22:14:10 +0100

    addTwoDigits solution

--- addTwoDigits/addTwoDigits.js
+++ addTwoDigits/addTwoDigits.js
@@ -1,4 +1,6 @@
 function addTwoDigits(n) {
+    const arr = n.toString().split('');
+    return arr.reduce((acc, digit) => parseInt(acc) + parseInt(digit));
 }
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
 //# sourceMappingURL=addTwoDigits.js.map
--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,9 @@
 function addTwoDigits(n: any): number {
+  const arr = n.toString().split('')
 
+  return arr.reduce(
+    (acc: string, digit: string) => parseInt(acc) + parseInt(digit)
+  )
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29))
