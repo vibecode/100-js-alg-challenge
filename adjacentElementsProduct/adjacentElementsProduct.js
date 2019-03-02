5bfd1391f5f8a39013a2f2e02453cf7c53664940
vibecode <rumashkin@gmail.com>
2019-03-02 23:11:08 +0100

    adjucent product solution

--- adjacentElementsProduct/adjacentElementsProduct.js
+++ adjacentElementsProduct/adjacentElementsProduct.js
@@ -1,4 +1,10 @@
 function adjacentElementsProduct(inputArray) {
+    const products = inputArray.reduce((acc, number, i) => {
+        const next = inputArray[i + 1];
+        return next ? [...acc, number * next] : acc;
+    }, []);
+    products.sort((a, b) => b - a);
+    return products[0];
 }
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
 //# sourceMappingURL=adjacentElementsProduct.js.map
--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+  const products = inputArray.reduce((acc, number, i) => {
+    const next = inputArray[i + 1]
+    return next ? [...acc, number * next] : acc
+  }, [])
 
+  products.sort((a, b) => b - a)
+
+  return products[0]
 }
 
-console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]))
