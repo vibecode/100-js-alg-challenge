cb998d465fb3243b17d68d3e11b42a89b6c29024
vibecode <rumashkin@gmail.com>
2019-03-03 13:52:52 +0100

    all longest strings solution

--- allLongestStrings/allLongestStrings.js
+++ allLongestStrings/allLongestStrings.js
@@ -1,4 +1,12 @@
 function allLongestStrings(inputArray) {
+    let longestLength = 0;
+    inputArray.forEach(str => {
+        const length = str.length;
+        if (length > longestLength) {
+            longestLength = length;
+        }
+    });
+    return inputArray.filter(str => str.length === longestLength);
 }
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba']));
 //# sourceMappingURL=allLongestStrings.js.map
--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,14 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  let longestLength = 0
 
+  inputArray.forEach(str => {
+    const length = str.length
+    if (length > longestLength) {
+      longestLength = length
+    }
+  })
+
+  return inputArray.filter(str => str.length === longestLength)
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba']))
