e576a04e39b138a9b55cfbbab21ffcb66fd00c06
vibecode <rumashkin@gmail.com>
2019-03-06 21:02:11 +0100

    alternating sums solution

--- alternatingSums/alternatingSums.js
+++ alternatingSums/alternatingSums.js
@@ -1,4 +1,12 @@
 function alternatingSums(a) {
+    return a.reduce((acc, weight, i) => {
+        if (i % 2 === 0) {
+            acc[0] !== undefined ? (acc[0] = acc[0] + weight) : (acc[0] = weight);
+            return acc;
+        }
+        acc[1] !== undefined ? (acc[1] = acc[1] + weight) : (acc[1] = weight);
+        return acc;
+    }, []);
 }
 console.log(alternatingSums([50, 60, 60, 45, 70]));
 //# sourceMappingURL=alternatingSums.js.map
--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,12 @@
 function alternatingSums(a: number[]): number[] {
-
+  return a.reduce((acc, weight, i) => {
+    if (i % 2 === 0) {
+      acc[0] !== undefined ? (acc[0] = acc[0] + weight) : (acc[0] = weight)
+      return acc
+    }
+    acc[1] !== undefined ? (acc[1] = acc[1] + weight) : (acc[1] = weight)
+    return acc
+  }, [])
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
