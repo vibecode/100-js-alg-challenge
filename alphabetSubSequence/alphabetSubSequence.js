8b48ee5ea0f681c74e43d68131fc9ac08022f75a
vibecode <rumashkin@gmail.com>
2019-03-06 15:31:03 +0100

    alphabet subsequence solution

--- alphabetSubSequence/alphabetSubSequence.js
+++ alphabetSubSequence/alphabetSubSequence.js
@@ -1,8 +1,19 @@
 function alphabetSubsequence(s) {
+  //'a' charCode = 97
+  //'z' charcode = 122
+  for (let i = 0; i < s.length; i++) {
+    if (
+      s.charCodeAt(i) < 97 ||
+      s.charCodeAt(i) > 122 ||
+      s.charCodeAt(i) >= s.charCodeAt(i + 1)
+    )
+      return false
+  }
+  return true
 }
-console.log(alphabetSubsequence('zab'));
-console.log(alphabetSubsequence('effg'));
-console.log(alphabetSubsequence('cdce'));
-console.log(alphabetSubsequence('ace'));
-console.log(alphabetSubsequence('bxz'));
+console.log(alphabetSubsequence('zab'))
+console.log(alphabetSubsequence('effg'))
+console.log(alphabetSubsequence('cdce'))
+console.log(alphabetSubsequence('ace'))
+console.log(alphabetSubsequence('bxz'))
 //# sourceMappingURL=alphabetSubSequence.js.map
--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,17 @@
 function alphabetSubsequence(s: string): boolean {
+  //'a' charCode = 97
+  //'z' charcode = 122
 
+  for (let i = 0; i < s.length; i++) {
+    if (
+      s.charCodeAt(i) < 97 ||
+      s.charCodeAt(i) > 122 ||
+      s.charCodeAt(i) >= s.charCodeAt(i + 1)
+    )
+      return false
+  }
+
+  return true
 }
 
 console.log(alphabetSubsequence('zab'))
